8937b3718e229483d12a740b2dbf74b17e2e7e77
Kharnael <parsyquentin@gmail.com>
2022-09-19 01:25:13 +0200

    pfiou

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,14 @@
 function toggleMute() {
-    var button=document.getElementById("MuteButton")
-    var video=document.getElementById("bgVideo")
-    video.volume = 0.5;
-    
-    if(video.muted){
-      video.muted = false;
-    } else {
-      video.muted = true;
-    }
-    
-    button.classList.toggle('muted');
+  var button = document.getElementById("MuteButton")
+  var video = document.getElementById("bgVideo")
+  video.volume = 0.5;
+
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
   }
 
-  /* validation formulaire */
-  
+  button.classList.toggle('muted');
+}
 
-  // OLD TV
